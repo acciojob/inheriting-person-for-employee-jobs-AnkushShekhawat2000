e023f06528f9ebbb9cec59154c10aa41dd418539
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2024-01-01 19:33:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// complete this js code
 function Person(name, age) {
 	this.name= name;
 	this.age= age;
@@ -15,3 +16,6 @@ Employee.prototype._proto_= Person.prototype;
 Employee.prototype.jobGreet= function(){
 	console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
 }
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
