d36575e9587457477b56662fff93f3122cee5fbf
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2024-01-01 18:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function(){
+	console.log("Hello, my name is ${this.name}, I am ${this.age} years old");
+}
 
+
+function Employee(name, age, jobTitle) {
+	Person.call(this, name,  age);
+	this.jobTitle = jobTitle;
+}
+
+Employee.protype = Object.create(Person.prototype);
+
+Employee.protype.jobGreet = function (){
+	console.log("Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.")
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
